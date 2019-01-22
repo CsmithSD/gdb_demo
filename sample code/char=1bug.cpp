7925992361e836ce2a07aeef0507632d30dc60b6
Shradha, Shashwati <shashwati.shradha@mines.sdsmt.edu>
2019-01-22 16:55:48 -0700

    Adding char c=1 bug

---
+++ sample code/char=1bug.cpp
@@ -0,0 +1,16 @@
+#include<string>
+#include<iostream>
+using namespace std;
+
+int main(){
+    string s="010100001111";
+    for(int i=0; i<s.length(); i++){
+        cout<<s[i]<<" :";
+        if(s[i] == 0){
+            cout<<"It's a ZERO"<<endl;
+        } else {
+            cout<<"It's a ONE"<<endl;
+        }
+    }
+    return 0;
+}
